17b416b024068601e8176a32eb59b5d265344480
Danyl EL-KABIR <elkabirdanyl@gmail.com>
2020-10-01 16:15:37 +0200

    on continue

--- src/main.c
+++ src/main.c
@@ -1,11 +1,15 @@
 #include "../headers/analyseur.h"
 void callback(unsigned char *args, const struct pcap_pkthdr *header, const unsigned char *packet) {
     (void)args;
-    (void)packet;
-    printf("caplen = %d\n", header->caplen);
+    unsigned i;
+    for (i = 0; i < header->caplen; i++) {
+        printf("%x ", packet[i]);
+    }
+    printf("time = %ld\n", header->ts.tv_sec);
 }
 
 int main (int argc, char **argv) {
+    printf("Bonjour Danyl\n");
     if ((argc-1)%2!=0) {
         printf("wrong number of arguments %d\n",argc-1);
         return EXIT_FAILURE;
@@ -13,12 +17,12 @@ int main (int argc, char **argv) {
     char *interface = NULL;
     char *fichier = NULL;
     char *filtre = NULL;
-    int verbose = -1;
+    int level = -1;
     int i;
     for(i = 1; i < argc; i += 2) {
         switch (argv[i][0]) {
               case '-':
-                if (strcmp(argv[i],"-i") == 0) {
+                if (strcmp(argv[i], "-i") == 0) {
                     if ((interface != NULL)||(fichier != NULL)) {
                         printf("Can't define interface -i\n");
                         return EXIT_FAILURE;
@@ -26,7 +30,7 @@ int main (int argc, char **argv) {
                     interface = argv[i+1];
                     printf("Interface %s\n",interface);
                 }
-                if(strcmp(argv[i],"-o") == 0) {
+                if(strcmp(argv[i], "-o") == 0) {
                     if((interface != NULL) || (fichier != NULL) || (filtre != NULL)) {
                         printf("Can't define offline file -o\n");
                         return EXIT_FAILURE;
@@ -43,12 +47,12 @@ int main (int argc, char **argv) {
                     printf("Filtre %s\n", filtre);
                 }
                 if (strcmp(argv[i], "-v") == 0) {
-                    if (verbose != -1) {
-                        printf("Can't define verbose -f\n");
+                    if (level != -1) {
+                        printf("Can't define level -f\n");
                         return EXIT_FAILURE;
                     }
-                    verbose = atoi(argv[i+1]);
-                    printf("Verbose %d\n", verbose);
+                    level = atoi(argv[i+1]);
+                    printf("level %d\n", level);
                 }
                 break;
               default:
@@ -85,7 +89,7 @@ int main (int argc, char **argv) {
         return EXIT_FAILURE;
     }
     int loop_res;
-    if ((loop_res = pcap_loop(packet, -1, callback, NULL)) == PCAP_ERROR) {
+    if ((loop_res = pcap_loop(packet, 1, callback, NULL)) == PCAP_ERROR) {
         fprintf(stderr, "pcap_loop error\n");
         return EXIT_FAILURE;
     }
