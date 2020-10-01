81571621de76cd67977b792a87d892a700202b17
Danyl EL-KABIR <elkabirdanyl@gmail.com>
2020-10-01 15:55:01 +0200

    ça avance

--- headers/analyseur.h
+++ headers/analyseur.h
@@ -1,3 +1,10 @@
 #include <pcap.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+typedef struct dName {
+    char *name;
+    struct dName *next;
+} dName;
--- src/main.c
+++ src/main.c
@@ -1,7 +1,95 @@
 #include "../headers/analyseur.h"
+void callback(unsigned char *args, const struct pcap_pkthdr *header, const unsigned char *packet) {
+    (void)args;
+    (void)packet;
+    printf("caplen = %d\n", header->caplen);
+}
 
 int main (int argc, char **argv) {
-    (void)argc;
-    (void)argv;
+    if ((argc-1)%2!=0) {
+        printf("wrong number of arguments %d\n",argc-1);
+        return EXIT_FAILURE;
+    }
+    char *interface = NULL;
+    char *fichier = NULL;
+    char *filtre = NULL;
+    int verbose = -1;
+    int i;
+    for(i = 1; i < argc; i += 2) {
+        switch (argv[i][0]) {
+              case '-':
+                if (strcmp(argv[i],"-i") == 0) {
+                    if ((interface != NULL)||(fichier != NULL)) {
+                        printf("Can't define interface -i\n");
+                        return EXIT_FAILURE;
+                    }
+                    interface = argv[i+1];
+                    printf("Interface %s\n",interface);
+                }
+                if(strcmp(argv[i],"-o") == 0) {
+                    if((interface != NULL) || (fichier != NULL) || (filtre != NULL)) {
+                        printf("Can't define offline file -o\n");
+                        return EXIT_FAILURE;
+                    }
+                    fichier = argv[i+1];
+                    printf("Fichier %s\n",fichier);
+                }
+                if (strcmp(argv[i], "-f")==0) {
+                    if ((filtre != NULL) || (fichier != NULL)) {
+                        printf("Can't define filter -f\n");
+                        return EXIT_FAILURE;
+                    }
+                    filtre = argv[i+1];
+                    printf("Filtre %s\n", filtre);
+                }
+                if (strcmp(argv[i], "-v") == 0) {
+                    if (verbose != -1) {
+                        printf("Can't define verbose -f\n");
+                        return EXIT_FAILURE;
+                    }
+                    verbose = atoi(argv[i+1]);
+                    printf("Verbose %d\n", verbose);
+                }
+                break;
+              default:
+                printf("Invalid arguments\n");
+            }
+    }
+
+    pcap_if_t *alldevs;
+    char errbuff[PCAP_ERRBUF_SIZE];
+    if (pcap_findalldevs(&alldevs, errbuff) == -1) {
+        fprintf(stderr, "Impossible de capturer les devices\n");
+        return EXIT_FAILURE;
+    }
+    bool exist = false;
+    while (alldevs != NULL) {
+        printf("%s\n", alldevs->name);
+        if (strcmp(alldevs->name, interface) == 0)
+            exist = true;
+        alldevs = alldevs->next;
+    }
+    if (!exist) {
+        fprintf(stderr, "%s doens\'t exist\nVoici la liste des interfaces disponibles :\n", interface);
+        return EXIT_FAILURE;
+    }
+    bpf_u_int32 netp, maskp;
+    if (pcap_lookupnet(interface != NULL ? interface : alldevs[0].name, &netp, &maskp, errbuff) == PCAP_ERROR) {
+        fprintf(stderr, "pcap_lookupnet error\n");
+        return EXIT_FAILURE;
+    }
+
+    pcap_t *packet;
+    if ((packet = pcap_open_live(interface != NULL ? interface : alldevs[0].name, BUFSIZ, 0, 1000, errbuff)) == NULL) {
+        fprintf(stderr, "pcap_open_live error\n");
+        return EXIT_FAILURE;
+    }
+    int loop_res;
+    if ((loop_res = pcap_loop(packet, -1, callback, NULL)) == PCAP_ERROR) {
+        fprintf(stderr, "pcap_loop error\n");
+        return EXIT_FAILURE;
+    }
+    printf("%d\n", loop_res);
+    pcap_close(packet);
     return EXIT_SUCCESS;
 }
