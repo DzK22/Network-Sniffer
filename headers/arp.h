a5b1281ad6ac4ce4c4a0131c2fc87773e0312c50
Danyl <elkabirdanyl@gmail.com>
2020-12-03 14:41:20 +0100

    suppr coms inutiles

--- headers/arp.h
+++ headers/arp.h
@@ -2,18 +2,6 @@
 #define ARP_H
 #include "network.h"
 
-/*struct c_arphdr {
-    unsigned short int ar_hrd;
-    unsigned short int ar_pro;
-    unsigned char ar_hln;
-    unsigned char ar_pln;
-    unsigned short int ar_op;
-    struct ether_addr ar_sha;
-    struct in_addr ar_sip;
-    struct ether_addr ar_tha;
-    struct in_addr ar_tip;
-};*/
-
 void treat_arp(const unsigned char *, int);
 void put_arp_opcode (int);
 
