8c11ddd095c931898300c7fb8530b847fc796958
Danyl <elkabirdanyl@gmail.com>
2020-12-11 11:54:13 +0100

    yyoyo

--- headers/dhcp.h
+++ headers/dhcp.h
@@ -1,7 +1,6 @@
 #ifndef DHCP_H
 #define DHCP_H
 #include "application.h"
-#define IP_TYPE {TAG_SUBNET_MASK, TAG_SERVER_ID}
 
 void treat_bootp (const unsigned char *, int);
 void print_dhcp (const unsigned char *, int);
